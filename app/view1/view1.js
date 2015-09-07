c8aa9ab0a35d5ae9a190503bf2860d4024a46e7c
gary <garyguo.works@gmail.com>
2015-09-07 11:43:55 +1000

    added simple angular feature examples

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -7,6 +7,19 @@ angular.module('myApp.view1', ['ngRoute'])
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl'
   })
-]).controller('View1Ctrl', [() => {
+]).controller('View1Ctrl', ['$scope', '$location', ($scope, $location) => {
+    let people = [
+        {name: 'Cathy', title: 'Analyst'},
+        {name: 'Alice', title: 'Designer'},
+        {name: 'Betty', title: 'Programmer'}
+    ];
 
+    $scope.inputData = '';
+
+    $scope.people = people;
+
+    $scope.sortByName = desc => $scope.people.sort((a, b) => a.name > b.name ? (!!desc ? 1 : -1) : (!!desc ? -1 : 1));
+    $scope.sortByTitle = desc => $scope.people.sort((a, b) => a.title < b.title ? (!!desc ? 1 : -1) : (!!desc ? -1 : 1));
+
+    $scope.routeInfo = () => $location.path();
 }]);
